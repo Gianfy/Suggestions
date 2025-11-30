08823b62963bb7a48d26f84f75e47751ffd7c0bc
jrondine <jrondine@gmail.com>
2025-11-30 08:14:37 +0100

    comments translated to english

--- recommendations.py
+++ recommendations.py
@@ -4,28 +4,28 @@ from dataset import restaurants, categories
 
 
 
-# Classe app raccomandazioni che prende ccome parametri i dati del dataset: categorie e elementi per ogni categoria
+# Recommendation app class that takes the dataset data as parameters: categories and elements for each category
 class Recommadations:
     def __init__(self, categories, items):
         self.categories = categories
         self.items = items
         
 
-    # restituisce una lista di categorie partendo da una lettera iniziale
+    # returns a list of categories starting from an initial letter choiced by user
     def get_categories(self):
         letter = self.choice_category_by_user()
         while letter == "":
             letter = self.choice_category_by_user()
         return [element for element in self.categories if element.startswith(letter)]
 
-    # restituisce una lista di liste degli elementi per ogni categoria inserita
+    # returns a list of element lists for each category inserted
     def get_iems(self, category):
         return [self.items[category] for category in self.items]    
 
-    # chiede all'utente di inserire una lettera per cercare le categorie
+    # asks the user to enter a letter to search for categories
     def choice_category_by_user(self):
         print("\nWhat type of food do you like to eat?")
-        letter = input("Enter the beginning letters of the categpories of food you want to choose: \n")
+        letter = input("Enter the beginning letters of the categories of food you want to choose: \n")
         return letter.capitalize()
 
     def start_use(self):
@@ -41,11 +41,11 @@ class Recommadations:
                 choice = input(f"Do you want to look a {results[0]} restaurants? Enter 'y' for yes and 'n' for no:\n")
                 if choice == "y":
                     # print(self.items[results[0]])
-                    print("The best 5 restaurants in Rome for your choice are:\n")
+                    print("\nThe best 5 restaurants in Rome for your choice are:\n")
                     self.show_all_items(results[0].lower())
-                    again = input("Do you want find other restaurants? Enter 'y' for yes and 'n' for no:\n")
+                    again = input("\nDo you want find other restaurants? Enter 'y' for yes and 'n' for no:\n")
                 else:
-                    again = input("Do you want find other restaurants? Enter 'y' for yes and 'n' for no:\n")
+                    again = input("\nDo you want find other restaurants? Enter 'y' for yes and 'n' for no:\n")
 
                 if again == 'n':
                     break
@@ -61,7 +61,7 @@ class Recommadations:
             print("Price:   {0}".format(item[2]))
             print("Address:  {0}".format(item[3]))
             
-    # semplicemente da il benvenuto all'user che sta usando l'app
+    # it simply welcomes the user who is using the app
     def greet(self):
         print("*********************************************************")
         print("* Welcome to our restaurat's recommadatios app for Rome *")
